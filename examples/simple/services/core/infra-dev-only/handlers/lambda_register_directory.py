d16f5a091f58ef9ca9d0f9aa940d221a43cc9e87
willyguggenheim <willy@gugcorp.com>
2022-07-03 11:05:19 -0700

    wip

---
+++ examples/simple/services/core/infra-dev-only/handlers/lambda_register_directory.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+
+import boto3
+
+client = boto3.client("workspaces")
+
+response = client.register_workspace_directory(
+    DirectoryId="{{resolve:ssm:/kubify/env/dev/infra/workspaces/directory_id}}",  # The directory ID, pulled from ssm, required.
+    EnableWorkDocs=False,  # Required.
+)
