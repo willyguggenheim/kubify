24c20b6579ebb6ed231e6267e3ad4cce876bda61
willyguggenheim <willy@gugcorp.com>
2022-07-06 06:52:00 -0700

    wip

--- terraform/modules/cloud-gcp/gke/modules/gcp-gke/examples/node_pool/data/shutdown-script.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-
-
-kubectl --kubeconfig=/var/lib/kubelet/kubeconfig drain --force=true --ignore-daemonsets=true --delete-local-data "$HOSTNAME"
--- terraform/modules/cloud-gcp/gke/modules/gcp-gke/examples/node_pool_update_variant_public_beta/data/shutdown-script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-
-
-kubectl --kubeconfig=/var/lib/kubelet/kubeconfig drain --force=true --ignore-daemonsets=true --delete-local-data "$HOSTNAME"
