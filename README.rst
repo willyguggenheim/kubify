2df1ac7992c8536e2876b7bbf157a46f122025c8
willyguggenheim <willy@gugcorp.com>
2022-10-08 13:03:46 -0700

    Bump version: 9010.0.19 → 9010.1.0

--- README.rst
+++ README.rst
@@ -1,7 +1,7 @@
 Welcome!
 ========
 
-Kubify OS, Version: 9010.0.19
+Kubify OS, Version: 9010.1.0
 
 Well hello there! Welcome to Kubify OS Python Package Repo
 
--- setup.py
+++ setup.py
@@ -71,6 +71,6 @@ setup(
     tests_requires=test_requirements,
     extras_require=extras,
     url="https://github.com/willyguggenheim/kubify",
-    version="9010.0.19",
+    version="9010.1.0",
     zip_safe=False,
 )
