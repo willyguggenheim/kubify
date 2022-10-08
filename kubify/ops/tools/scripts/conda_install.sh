035717e55e87859e4027fe01d3c319297092530a
willyguggenheim <willy@gugcorp.com>
2022-10-08 13:54:43 -0700

    up, start, make & conda pipeline

--- kubify/ops/tools/scripts/conda_install.sh
+++ kubify/ops/tools/scripts/conda_install.sh
@@ -5,4 +5,8 @@ mkdir -p ./kubify_tools
 curl -o ./kubify_tools/miniconda-install-aarch64.sh "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh"
 curl -o ./kubify_tools/miniconda-install-x86_64.sh "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
 chmod +x ./kubify_tools/miniconda-install*.sh
-echo $OSTYPE | grep arwin || ./kubify_tools/miniconda-install-aarch64.sh -b -p /opt/conda || ./kubify_tools/miniconda-install-x86_64.sh -b -p /opt/conda
+uname -m | grep arm && ./kubify_tools/miniconda-install-aarch64.sh -b -p /opt/conda || true
+uname -m | grep aarch && ./kubify_tools/miniconda-install-aarch64.sh -b -p /opt/conda || true
+uname -m | grep amd && ./kubify_tools/miniconda-install-x86_64.sh -b -p /opt/conda || true
+uname -m | grep x86_64 && ./kubify_tools/miniconda-install-x86_64.sh -b -p /opt/conda || true
+conda info
