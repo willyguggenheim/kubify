d58faaff632684434f672bc9a85710737006fd73
willyguggenheim <willy@gugcorp.com>
2023-01-28 20:19:31 -0800

    Bump version: 9020.0.12 → 9020.0.13

--- setup.py
+++ setup.py
@@ -73,6 +73,6 @@ setup(
     tests_require=test_requirements,
     extras_require=extras,
     url="https://github.com/willyguggenheim/kubify",
-    version="9020.0.12",
+    version="9020.0.13",
     zip_safe=False,
 )
